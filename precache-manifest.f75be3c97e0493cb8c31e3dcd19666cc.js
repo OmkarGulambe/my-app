df06b071a19c7752408282db8e4e558e9393eb75
Omkar Ramesh Gulambe <omkarrg2002@gmail.com>
2023-04-07 23:09:39 +0530

    Updates

---
+++ precache-manifest.f75be3c97e0493cb8c31e3dcd19666cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac9c21e67e0fc50800cc",
+    "url": "/my-app/static/css/main.b17d33ab.chunk.css"
+  },
+  {
+    "revision": "ac9c21e67e0fc50800cc",
+    "url": "/my-app/static/js/main.ac9c21e6.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/my-app/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/my-app/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8b16e4071cc3418912ee",
+    "url": "/my-app/static/js/runtime~main.8b16e407.js"
+  },
+  {
+    "revision": "721c058989de7856465f9df8b1d59b1a",
+    "url": "/my-app/index.html"
+  }
+];
